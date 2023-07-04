bd6db6a2a65edc8a73b35e61affad5c06805f253
KnopMops <KnopMops@yandex.ru>
2023-07-04 16:36:13 +0300

    ByOnet Launcher

---
+++ ByOnet_Games/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ByOnet_Games
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(261, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.UpdateGame);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(-1, 80);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(256, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 150);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ByOnet Игры - Самый лучший лаунчер для игр";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ByOnet_Games/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+using System.Net;
+using System.Text;
+using System.IO.Compression;
+using System.Diagnostics;
+
+namespace ByOnet_Games
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string version = "";
+        public string serverVersion = "";
+        public string path = @"Game\";
+        public string exename = "Game";
+        public string url = "http://192.168.1.60/ByOnet/";
+        public bool downloading = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(Path.GetFullPath(path));
+            if (!File.Exists(Path.GetFullPath(path + exename)))
+            {
+                RegistryKey key;
+                key = Registry.CurrentUser.CreateSubKey("Shooter");
+                key.SetValue("version", "none");
+                key.Close();
+                version = "none";
+            }
+            else
+            {
+                version = (string)Registry.CurrentUser.OpenSubKey("Shooter").GetValue("version");
+            }
+
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + "v.txt");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream recivestream = response.GetResponseStream();
+                StreamReader readstream = null;
+                if (response.CharacterSet == "null")
+                {
+                    readstream = new StreamReader(recivestream);
+                }
+                else
+                {
+                    readstream = new StreamReader(recivestream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                string data = readstream.ReadToEnd();
+                serverVersion = data;
+                response.Close();
+                readstream.Close();
+            }
+            else
+            {
+                MessageBox.Show("Ошибка соединения с сервером", "ByOnet");
+            }
+
+            label1.Text = version + "/" + serverVersion;
+
+            if (version != serverVersion)
+            {
+                if (File.Exists(Path.GetFullPath(path + exename)))
+                {
+                    button1.Text = "Обновить";
+                    button1.Click += UpdateGame;
+                }
+                else
+                {
+                    button1.Text = "Скачать";
+                    button1.Click += UpdateGame;
+                }
+            }
+            else
+            {
+                button1.Text = "Запуск";
+                button1.Click += Play;
+            }
+        }
+
+        private void UpdateGame(object sender, EventArgs e)
+        {
+            downloading = false;
+            button1.Enabled = false;
+            DirectoryInfo dir = new DirectoryInfo(Path.GetFullPath(path));
+            foreach (FileInfo item in dir.GetFiles())
+            {
+                item.Delete();
+            }
+            foreach (DirectoryInfo item in dir.GetDirectories())
+            {
+                item.Delete(true);
+            }
+
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += (s, g) =>
+                {
+                    if (g.ProgressPercentage == 100)
+                    {
+                        if (!File.Exists(Path.GetFullPath(path + exename)))
+                        {
+                            ZipFile.ExtractToDirectory("g.zip", path);
+                            RegistryKey key;
+                            key = Registry.CurrentUser.CreateSubKey("Shooter");
+                            key.SetValue("version", serverVersion);
+                            key.Close();
+                            version = serverVersion;
+                            label1.Text = version + "/" + serverVersion;
+
+                            button1.Click -= UpdateGame;
+                            button1.Click += Play;
+                        }
+                        button1.Enabled = true;
+                        downloading = false;
+                    }
+                };
+
+                wc.DownloadFileAsync(new Uri(url + "Game.zip"), "g.zip");
+            }
+        }
+
+        private void Play(object sender, EventArgs e)
+        {
+            Process.Start(path + exename);
+        }
+    }
+}
